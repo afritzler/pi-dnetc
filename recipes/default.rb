f875556b19622157a677638d8a511dbbc612f336
Andreas Fritzler <andreas.fritzler@gmail.com>
2014-08-11 22:39:55 +0200

    Fixed install dir setup

--- recipes/default.rb
+++ recipes/default.rb
@@ -12,17 +12,10 @@ remote_file "#{node['dnetc_cache_dir']}/#{node['dnetc_pgk']}" do
   source node["dnetc_url"]
 end
 
-# Create install directory
-directory node['dnetc_install_dir'] do
-  owner "root"
-  group "staff"
-  mode 00644
-  action :create
-end
-
 # Unzip the packge
 bash "untar the downloaded packege" do
   user "root"
+  group "staff"
   cwd node['dnetc_install_dir']
   code <<-EOH
   tar -zxf #{node['dnetc_cache_dir']}/#{node['dnetc_pgk']}
@@ -33,6 +26,7 @@ end
 if !File.exist?(node['dnetc_symlink'])
   bash "create symlink in bin folder" do
     user "root"
+    group "staff"
     cwd node['dnetc_install_dir']
     code <<-EOH
     ln -s #{node['dnetc_install_dir']}/#{node['dnetc_pgk']}/dnetc #{node['dnetc_symlink']}
